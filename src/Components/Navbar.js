a3f4dcafc5933a0473e6a4fae3c7529818593e1c
Swapnil Mall <swapnilmall03@gmail.com>
2020-07-26 18:16:37 +0530

    added reducer and actions

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { data } from '../data';
+
+function Navbar() {
+    return (
+        <div className="nav">
+            <div className="search-container">
+                <input />
+                <button id='search-btn'>Search</button>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+// {
+//     type: 'INCREASE_COUNT',
+//     movies: [m1,m2,m3] 
+// }
+
+// {
+//     type: 'DECREASE_COUNT'
+// }
